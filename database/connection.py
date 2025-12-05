de5393a2e088f66c05f250352e2e7dd0d1821b44
Andrews Rezende Farias <andrewsrezende@gmail.com>
2025-12-05 14:33:26 -0300

    Update files

--- database/connection.py
+++ database/connection.py
@@ -1,15 +1,16 @@
+import os
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 def get_db():
-    mongo_uri = MongoClient("MONGO_URI")
+    mongo_uri = os.getenv("MONGO_URI")
     if not mongo_uri:
         raise Exception("ERRO: Variável MONGO_URI não encontrada no ambiente!")
 
     client = MongoClient(mongo_uri, server_api=ServerApi('1'))
 
-    db = client["HelpTask"]
-    return db
+    # db = client["HelpTask"]
+    return client["HelpTask"]
 
 db = get_db()
 colecao = db["tarefas"]
